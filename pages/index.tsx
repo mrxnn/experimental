6cce64902e71417f6f64daaa97073ab2f8996c8e
Mayura <mayura.ramanayake@gmail.com>
2021-09-07 22:03:47 +0530

    CleanedUp

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Prisma App</title>
+        <meta name="description" content="Prisma test app" />
+      </Head>
+      <h1>Prisma App</h1>
+    </div>
+  );
+}
