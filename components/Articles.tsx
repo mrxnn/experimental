036dae4f165b670e94051cbb434484b7bff404bd
Mayura <mayura.ramanayake@gmail.com>
2021-09-09 03:04:12 +0530

    Thinner Borders

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -21,11 +21,11 @@ export default function Articles() {
 export function Article({ index, description, delay }) {
   return (
     <motion.div
-      className="flex items-center border-b-2 py-9"
+      className="flex items-center border-b py-9"
       initial={{ opacity: 0, translateY: 20 }}
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 0.4, delay: delay * 0.1 + 0.6 }}>
-      <p className="w-36">{index}</p>
+      <p className="w-36 md:w-52">{index}</p>
       <p className="flex-1">{description}</p>
     </motion.div>
   );
@@ -34,11 +34,11 @@ export function Article({ index, description, delay }) {
 export function ArticleHeader() {
   return (
     <motion.div
-      className="flex items-center border-b-2 py-9"
+      className="flex items-center border-b py-9"
       initial={{ opacity: 0, translateY: 20 }}
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 0.5, delay: 0.6 }}>
-      <p className="w-36">#</p>
+      <p className="w-36 md:w-52">#</p>
       <p className="flex-1">Name</p>
       <p className="rotate-90">&rarr;</p>
     </motion.div>
