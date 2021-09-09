0413eedb0229f56c79d44bf6d6d7b1f88be1c754
Mayura <mayura.ramanayake@gmail.com>
2021-09-09 05:40:34 +0530

    Remove Dark Mode

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="dark:bg-black dark:text-white overflow-hidden tracking-tight">
+    <>
       <Head>
         <title>Mayura Ramanayaka &mdash; (Index)</title>
       </Head>
@@ -11,9 +11,9 @@ export default function Layout({ children }) {
         <Navbar />
       </div>
       <div className="max-w-3xl mx-auto px-5 mt-[60vh]">{children}</div>
-      <div className="fixed bottom-0 left-0 right-0 h-20 bg-gradient-to-b from-transparent to-white dark:to-black">
+      <div className="fixed bottom-0 left-0 right-0 h-20 bg-gradient-to-b from-transparent to-black">
         &nbsp;
       </div>
-    </div>
+    </>
   );
 }
--- components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <div className="w-10 h-10 bg-gray-50 flex justify-center items-center">
-      <p className="text-black translate-x-[2px] translate-y-[1px]">M.</p>
-    </div>
-  );
-}
