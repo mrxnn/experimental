109e153714c379b44cdbe5d50b6aff556d1827d0
Mayura <mayura.ramanayake@gmail.com>
2021-09-23 06:36:28 +0530

    Made the window position relative

--- kbd-command/ui/Window.tsx
+++ kbd-command/ui/Window.tsx
@@ -32,7 +32,7 @@ const Window: FC<{ isOpen: boolean; setIsOpen: (value: boolean) => void }> = ({
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-40 sm:translate-y-0 sm:scale-95">
-              <div className="bg-white dark:bg-black border border-gray-100 dark:border-gray-700/60 w-[640px] sm:h-[423px] px-[1px] transform mt-[180px] shadow-2xl dark:shadow-xl rounded-lg relative">
+              <div className="bg-white dark:bg-black border border-gray-100 dark:border-gray-700/60 w-[640px] sm:h-[423px] px-[1px] transform mt-[20vh] shadow-2xl dark:shadow-xl rounded-none sm:rounded-lg relative">
                 {children}
               </div>
             </Transition.Child>
